b64ff9977b81687caf4bb561037e0544d96134a5
hustarico <hussamtarteer74@gmail.com>
2026-05-03 11:47:30 +0300

    reset no.3

---
+++ src/main/java/com/_4/ProjectPal/ProjectPalApplication.java
@@ -0,0 +1,13 @@
+package com._4.ProjectPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_4/ProjectPal/ProjectPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com._4.ProjectPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
